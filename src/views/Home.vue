8d3a7f2e40f269895f7d3f820c8774e7d4d7deee
Roberta <robertaf898@gmail.com>
2019-05-15 10:01:34 +0100

    create side menu

---
+++ src/views/Home.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="home">
+    <h1>Homepage</h1>
+  </div>
+       <!-- <v-btn class="hidden-md-and-up">click me</v-btn> -->
+        <!-- <v-btn class="hidden-sm-only">click me</v-btn> -->
+
+        <!-- <v-btn class="hidden-md-and-down">click me</v-btn> -->
+           
+
+      <!-- <p class="red white--text">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsa odit modi cum minima magnam optio dolores sunt dolore non veritatis velit aut, laborum perspiciatis? Esse cum quidem id quam ipsam.</p>
+      <p class="pink lighten-4 red--text text--darken-4">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsa odit modi cum minima magnam optio dolores sunt dolore non veritatis velit aut, laborum perspiciatis? Esse cum quidem id quam ipsam.</p>
+      <h1 class="display-4">Massive Display</h1>
+      <h4 class="display-1">Smaller Display</h4>
+      <p class="headline"> This is a headline</p>
+       <p class="subheading font-weight-bold"> This is a subheading</p>
+       <p class="caption"> This is a caption</p>  -->
+       <!-- <v-btn class="pink white--text"> click me</v-btn> background-color and text with shadow -->
+        <!-- <v-btn depressed color="pink"> click me</v-btn>   background-color without shadow -->
+        <!-- <v-btn flat color="pink"> click me</v-btn>   no shadow, transparent background and color pink. Hover and light pink background -->
+
+        <!-- <v-btn depressed class="pink white--text">
+            <v-icon left> email</v-icon>
+            <span> email me</span>
+        </v-btn>   
+
+        <v-btn depressed small class="pink white--text">
+            <v-icon left small> email</v-icon>
+            <span> email me</span>
+        </v-btn>
+
+        <v-btn depressed large class="pink white--text font-weight-bold">
+            <span> email me</span>
+            <v-icon right large> email</v-icon>
+        </v-btn>   
+
+        <v-btn fab depressed small dark color="purple">
+            <v-icon> favorite</v-icon>
+        </v-btn>    -->
+</template>
+
+<script>
+
+  export default {
+   
+  }
+</script>
